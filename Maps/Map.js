249653775cd397d2c99933a42498d5a41ca4a997
GabrielRalph <gltralph@gmail.com>
2021-09-07 11:44:35 +1000

    Rev 3: map labels centered using polylabel algorithm, recovery (30 days default)

---
+++ Maps/Map.js
@@ -0,0 +1,208 @@
+import {SvgPlus, Vector} from "../3.5.js"
+class Map extends SvgPlus{
+  constructor(id) {
+    super(id);
+    this.bgroup = new SvgPlus(this.children[0]);
+    this.labels = new SvgPlus(this.children[1]);
+  }
+
+  get boundries(){
+    return this.bgroup.children;
+  }
+
+  set_boundry_props(bprops, def) {
+    for (let boundry of this.boundries) {
+      if (!SvgPlus.is(boundry, Boundry)) {
+        boundry = new Boundry(boundry);
+      }
+      let pc = boundry.postcode;
+      let loc = boundry.locality;
+
+      if (pc in bprops) {
+        boundry.props = bprops[pc];
+      } else if (loc in bprops) {
+        boundry.props = bprops[loc];
+      } else {
+        boundry.props = def;
+      }
+    }
+  }
+  set_props(els, props) {
+    for (let el of els) {
+      if (!SvgPlus.is(el, SvgPlus)) {
+        el = new SvgPlus(el);
+      }
+      el.props = props;
+    }
+  }
+
+  set_loc_props(locality, props) {
+    let els = this.b_by_pc(locality);
+    this.set_props(els, props);
+  }
+  set_pc_props(postcode, props) {
+    let els = this.b_by_pc(postcode);
+    this.set_props(els, props);
+  }
+
+  b_by_pc(postcode) {
+    return this.bgroup.getElementsByClassName(postcode);
+  }
+  b_by_loc(locality) {
+    locality = locality.replace(/\s+/g, "-");
+    return this.bgroup.getElementsByClassName(locality);
+  }
+
+  label_by_pc(postcode) {
+    return this.labels.getElementsByClassName(postcode);
+  }
+  label_by_loc(locality) {
+    locality = locality.replace(/\s+/g, "-");
+    return this.labels.getElementsByClassName(locality);
+  }
+
+  static getBoundryClassName(boundry) {
+    let loc = boundry.props.nsw_loca_2.replace(/^\s*/, "");
+    loc = loc.replace(/\s+/g, "-");
+    return boundry.props.postcode + " " + loc;
+  }
+}
+
+class Boundry extends SvgPlus{
+  get postcode() {
+    return this.getAttribute("postcode");
+  }
+  get locality() {
+    return this.getAttribute("locality");
+  }
+}
+
+// let Ladjust = {
+//   "GUILDFORD": {
+//     pos: new Vector(0, -0.6),
+//   },
+//   "MERRYLANDS": {
+//     pos: new Vector(0.31, 0.31),
+//     w: 0.9,
+//   },
+//   "GREYSTANES": {
+//     pos: new Vector(0.8, 0),
+//   },
+//   "AUBURN": {
+//     pos: new Vector(-0.2, -0.2)
+//   },
+//   "HAWKESBURY HEIGHTS": {
+//     pos: new Vector(0, -0.5),
+//     w: 1.4
+//   },
+//   "YARRAMUNDI": {
+//     pos: new Vector(-1.2, -1),
+//   },
+//   "CASTLEREAGH": {
+//     pos: new Vector(-0.4, 0),
+//   },
+//   "PENRITH": {
+//     pos: new Vector(1.1, 0),
+//   },
+//   "EMU PLAINS": {
+//     pos: new Vector(0.5, -0.3),
+//   },
+//   "EMU HEIGHTS": {
+//     pos: new Vector(0.1, -0.7),
+//     w: 1.2
+//   },
+//   "MOUNT RIVERVIEW": {
+//     pos: new Vector(-0.3, 0.26)
+//   },
+//   "GLENBROOK": {
+//     pos: new Vector(0, -0.5)
+//   },
+//   "LAPSTONE": {
+//     pos: new Vector(0, -0.2)
+//   },
+//   "LEONAY": {
+//     pos: new Vector(0, -0.6)
+//   },
+//   "REGENTVILLE": {
+//     pos: new Vector(-0.15, 0)
+//   },
+//   "JAMISONTOWN": {
+//     pos: new Vector(0, -0.5)
+//   },
+//   "GLENMORE PARK": {
+//     pos: new Vector(0, -0.5)
+//   },
+//   "CRANEBROOK": {
+//     pos: new Vector(0.3, -1)
+//   },
+//   "LONDONDERRY": {
+//     pos: new Vector(-0.35, 0)
+//   },
+//   "LLANDILO": {
+//     pos: new Vector(-.05, -1.2),
+//     // w: 0.8
+//   },
+//   "JORDAN SPRINGS": {
+//     pos: new Vector(-.6, 0.3),
+//   },
+//   "MASCOT": {
+//     pos: new Vector(0, -1.7)
+//   },
+//   "BOTANY": {
+//     pos: new Vector(0.2, -0.2)
+//   },
+//   "PAGEWOOD": {
+//     pos: new Vector(1.2, -0.2),
+//     w: 0.5
+//   }
+//
+// }
+// class MapLabel extends SvgPlus{
+//   constructor(boundry){
+//     super("g");
+//     let locality = boundry.locality;
+//     let bbox = boundry.getBBox();
+//
+//     let o = new Vector(bbox);
+//     let size = new Vector(bbox.width, bbox.height);
+//     let c = o.add(size.div(2));
+//
+//     let n = locality.length;
+//     let w = size.x / n;
+//     w = w / 1.5;
+//
+//     if (locality in Ladjust) {
+//       let a = Ladjust[locality];
+//
+//       c = c.add(a.pos);
+//       let aw = !a.w ? 1 : a.w;
+//       w *= aw;
+//     }
+//
+//
+//     this.center = c;
+//     this.scale = w;
+//     this.class = boundry.class;
+//     this.boundry = boundry;
+//   }
+// }
+// class CovidMapLabel extends MapLabel{
+//   constructor(boundry) {
+//     super(boundry);
+//
+//     let c = this.center;
+//     let s = this.scale;
+//
+//
+//     this.createChild("text", {
+//       x: c.x,
+//       y: c.y + s * 0.3,
+//       "text-anchor": "middle",
+//       "font-size": s,
+//       style: {
+//         "--ls": 1/s,
+//       }
+//     }).innerHTML = boundry.locality;
+//   }
+// }
+export {Map}
--- map_test.js
+++ map_test.js
@@ -1,20 +1,20 @@
-import {Vector} from "./3.5.js"
+import {SvgPlus, Vector} from "./3.5.js"
+import {Map} from "./Maps/Map.js"
+
 async function getJSON(url){
   let res = await fetch(url);
   let data = await res.json();
   return data;
 }
 
-function riskByPostcode(cbyp, time, recovery = 70){
+function riskByPostcode(cbyp, time, recovery = 30){
   let rct = recovery * 1000 * 60 * 60 * 24;
   let rbyp = {};
   for (let pc in cbyp) {
     let cases = cbyp[pc];
     let risk = 0;
     for (let c of cases) {
-      // console.log(c);
       if (c < time) {
-        // console.log(c - time);
         let r = 1 - (time - c)/(rct);
         r = r < 0 ? 0 : r;
         risk += r;
@@ -22,7 +22,6 @@ function riskByPostcode(cbyp, time, recovery = 70){
     }
     rbyp[pc] = risk;
   }
-
   return rbyp;
 }
 function numCasesTo(cases, to) {
@@ -50,191 +49,56 @@ function caseByPostcode(cases, from = 0) {
   return cbyp;
 }
 
+function addCasesLabel(label, cases) {
+  if (!SvgPlus.is(label, SvgPlus)) label = new SvgPlus(label);
 
-let adjust = {
-  "GUILDFORD": {
-    pos: new Vector(0, -0.6),
-  },
-  "MERRYLANDS": {
-    pos: new Vector(0.31, 0.31),
-    w: 0.9,
-  },
-  "GREYSTANES": {
-    pos: new Vector(0.8, 0),
-  },
-  "AUBURN": {
-    pos: new Vector(-0.2, -0.2)
-  },
-  "HAWKESBURY HEIGHTS": {
-    pos: new Vector(0, -0.5),
-    w: 1.4
-  },
-  "YARRAMUNDI": {
-    pos: new Vector(-1.2, -1),
-  },
-  "CASTLEREAGH": {
-    pos: new Vector(-0.4, 0),
-  },
-  "PENRITH": {
-    pos: new Vector(1.1, 0),
-  },
-  "EMU PLAINS": {
-    pos: new Vector(0.5, -0.3),
-  },
-  "EMU HEIGHTS": {
-    pos: new Vector(0.1, -0.7),
-    w: 1.2
-  },
-  "MOUNT RIVERVIEW": {
-    pos: new Vector(-0.3, 0.26)
-  },
-  "GLENBROOK": {
-    pos: new Vector(0, -0.5)
-  },
-  "LAPSTONE": {
-    pos: new Vector(0, -0.2)
-  },
-  "LEONAY": {
-    pos: new Vector(0, -0.6)
-  },
-  "REGENTVILLE": {
-    pos: new Vector(-0.15, 0)
-  },
-  "JAMISONTOWN": {
-    pos: new Vector(0, -0.5)
-  },
-  "GLENMORE PARK": {
-    pos: new Vector(0, -0.5)
-  },
-  "CRANEBROOK": {
-    pos: new Vector(0.3, -1)
-  },
-  "LONDONDERRY": {
-    pos: new Vector(-0.35, 0)
-  },
-  "LLANDILO": {
-    pos: new Vector(-.05, -1.2),
-    // w: 0.8
-  },
-  "JORDAN SPRINGS": {
-    pos: new Vector(-.6, 0.3),
-  },
-  "MASCOT": {
-    pos: new Vector(0, -1.7)
-  },
-  "BOTANY": {
-    pos: new Vector(0.2, -0.2)
-  },
-  "PAGEWOOD": {
-    pos: new Vector(1.2, -0.2),
-    w: 0.5
+  let text = label.children[1];
+  if (!text) {
+    text = label.createChild("text", {
+      y: 1,
+      "font-size": 1,
+      "text-anchor": "middle"
+    });
   }
-
+  text.innerHTML = `${cases} cases`
 }
-function addNames() {
-  let map = document.getElementById("map");
-  let boundries = document.getElementsByClassName("boundries")[0];
-  let labels = "";
-  for (let boundry of boundries.children) {
-    let bbox = boundry.getBBox();
-
-    let o = new Vector(bbox);
-    let size = new Vector(bbox.width, bbox.height);
-    let c = o.add(size.div(2));
-
-
-    let cname = boundry.getAttribute("class").split(" ");
-    let pc = cname[0];
-    let name = cname[1].replace("-", " ");
-
-
-    let n = name.length;
-    let w = size.x / n;
-    w = w / 1.5;
-
-    if (name in adjust) {
-      let a = adjust[name];
-      c = c.add(a.pos);
-      let aw = !a.w ? 1 : a.w;
-      w *= aw;
-    }
-
-    labels += `<text font-size = ${w} text-anchor = "middle" x = ${c.x} y = ${c.y + w*0.3}>
-                <tspan>${name}</tspan>
-                <tspan class = "${pc}-L1" font-size = "0.8em" x = ${c.x} y = "${c.y + w*1.3}">cases</tspan>
-                <tspan style = "--ls${1/w}" class = "${pc}-L2" font-size = "0.8em" x = ${c.x} y = "${c.y + w*2.3}"></tspan>
-              </text>`
-  }
-  map.innerHTML += `<g class = "labels">${labels}</g>`;
-}
-
-function setStyle(el, style){
-  for (let key in style) {
-    el.style.setProperty(key, style[key]);
-  }
-}
-function setProps(el, props) {
-  for (let key in props) {
-    if (key == "style") {
-      setStyle(el, props.style);
-    } else if (key == "html") {
-      el.innerHTML = props[key];
-    }else {
-      el.setAttribute(key, props[key]);
-    }
-  }
-}
-
-function setBoundryByPostcode(pc, props) {
-  let els = document.getElementsByClassName(pc);
-  for (let el of els) {
-    setProps(el, props);
-  }
-}
-function setL2ByPostcode(pc, props) {
-  let els = document.getElementsByClassName(pc + "-L2");
-  for (let el of els) {
-    setProps(el, props);
+function addCasesLabels(labels, cases) {
+  for (let label of labels) {
+    addCasesLabel(label, cases);
   }
 }
 
 let init = true;
-function setRisk(cases, now) {
+function setRisk(cases, now, map) {
   if (init) {
-    let boundries = document.getElementsByClassName("boundries")[0];
-    for (let child of boundries.children) {
-      child.style.setProperty("--risk", 0);
+    for (let boundry of map.boundries) {
+      boundry.style.setProperty("--risk", 0);
     }
   }
 
   let risk = riskByPostcode(cases, now);
   let numc = numCasesTo(cases, now);
   for (let pc in risk) {
-    let els = document.getElementsByClassName(pc);
-    for (let el of els) {
-      el.style.setProperty("--risk", risk[pc])
-    }
-    els = document.getElementsByClassName(pc+"-L2");
-    // console.log(els);
-    for (let el of els) {
-      // console.log(numc[pc]);
-
-      el.innerHTML = numc[pc]
-    }
+    map.set_pc_props(pc, {style: {
+        "--risk": risk[pc]
+      }
+    })
+    addCasesLabels(map.label_by_pc(pc), numc[pc]);
   }
 }
 
 
 let caseurl = "https://data.nsw.gov.au/data/api/3/action/datastore_search?resource_id=21304414-1ff1-4243-a5d2-f52778048b29&limit=50000";
 let load = async () => {
-  addNames();
   let cases = await getJSON(caseurl);
+  let map = new Map("map");
+
   cases = caseByPostcode(cases.result.records, new Date("1 Jun 2021").getTime());
 
   let today = new Date().getTime();
   let now = new Date("1 Jun 2021").getTime();
   let next = () => {
-    setRisk(cases, now);
+    setRisk(cases, now, map);
     now += 1000*60*60*24;
     if (now <= today) {
       console.log(now);
